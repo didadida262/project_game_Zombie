49ccd1e06533eb9e8cc4bf0b12dfe606e02224fd
didadida262 <719713496@qq.com>
2024-12-24 13:58:05 +0800

    game_zobie_init

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import cn from "classnames";
+import { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+
+import { useTranslation, switchLanguage } from "@/i18n";
+import customToast from "@/components/customToast";
+import Zombie from "@/pages/Zombie/index";
+
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const { t } = useTranslation();
+  useEffect(() => {
+    customToast.success("Mounted success!");
+  }, []);
+
+  return (
+    <div className={cn("h-screen w-full bg-bgPrimaryColor")}>
+      <Zombie />
+      {/* <ToastContainer
+        theme="dark"
+        autoClose={3000}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        draggable={false}
+        pauseOnHover={false}
+      />
+      <span>{t("slogan")}</span>
+      <ButtonCommon
+        type={EButtonType.SIMPLE}
+        onClick={() => {
+          const lan = localStorage.getItem("language");
+          switchLanguage(lan === "zh" ? "en-US" : "zh");
+        }}
+      >
+        <span>切换语言</span>
+      </ButtonCommon>
+      <ButtonTheme /> */}
+    </div>
+  );
+}
+
+export default App;
