c40472e71cb5783bed427bd1807e3783e1a319e7
didadida262 <719713496@qq.com>
2024-12-30 17:58:25 +0800

    modify_bomb

--- src/pages/Zombie/game/scenes/Preloader.ts
+++ src/pages/Zombie/game/scenes/Preloader.ts
@@ -16,23 +16,10 @@ export class Preloader extends Scene {
   }
   init() {
     // //  We loaded this image in our Boot Scene, so we can display it here
-    // this.add.image(512, 384, "background");
-    // //  A simple progress bar. This is the outline of the bar.
-    // this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
-    // //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    // const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
-    // //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-    // this.load.on("progress", (progress: number) => {
-    //   //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-    //   bar.width = 4 + 460 * progress;
-    // });
   }
 
   preload() {
     // //  Load the assets for the game - Replace with your own assets
-
-    // this.load.image("logo", "logo.png");
-    // this.load.image("star", "star.png");
     this.load.image("sky", "assets/sky.png");
     this.load.image("ground", "assets/platform.png");
     this.load.image("star", "assets/star.png");
@@ -52,11 +39,6 @@ export class Preloader extends Scene {
     this.score += 10;
     this.scoreText.setText("Score: " + this.score);
     if (this.stars.countActive(true) === 0) {
-      //   this.stars.children.iterate((child: any) => {
-      //     if (child.enableBody) {
-      //       child.enableBody(true, child.x, 0, true, true);
-      //     }
-      //   });
       this.stars.children.iterate((child: any) =>
         child.enableBody(Phaser.Math.FloatBetween(0.4, 0.8)),
       );
@@ -76,10 +58,10 @@ export class Preloader extends Scene {
     player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody,
     bomb: Phaser.Physics.Arcade.Group,
   ) {
-    if (!this.player) return;
+    if (!player) return;
     this.physics.pause();
-    this.player.setTint(0xff0000);
-    this.player.anims.play("turn");
+    player.setTint(0xff0000);
+    player.anims.play("turn");
     this.gameOver = true;
   }
   create() {
@@ -130,12 +112,6 @@ export class Preloader extends Scene {
       repeat: 11,
       setXY: { x: 12, y: 0, stepX: 70 },
     });
-
-    // this.stars.children.iterate(function (
-    //   child: Phaser.Structs.Set<Phaser.GameObjects.GameObject>,
-    // ) {
-    //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    // });
     this.stars.children.iterate((child: any) =>
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)),
     );
@@ -164,7 +140,7 @@ export class Preloader extends Scene {
     );
   }
   update() {
-    console.log("update>>>>");
+    if (this.gameOver) return;
     if (!this.cursors || !this.player) return;
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-160);
